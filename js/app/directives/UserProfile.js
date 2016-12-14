5d9c0b431aba1f23531afb0ecc8fbd68319bb50d
Steve Frost <ffrostt@gmail.com>
2016-12-13 22:06:22 -0500

    converted transclusion into object to insert headers before each user item

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -1,13 +1,22 @@
 function UserProfile() {
 	return {
-		transclude: true,
+		transclude: {
+			name: '?h4',
+			position: '?h6',
+			description: '?p'
+		},
 		template: [
 			'<div>',
 				'<h3>User Profile</h3>',
-				'<div ng-transclude></div>',
+				'<h4>Name</h4>',
+				'<div><span ng-transclude="name">No name</span></div>',
+				'<h6>Position</h6>',
+				'<div><span ng-transclude="position">No position</span></div>',
+				'<h6>Description</h6>',
+				'<div><span ng-transclude="description">No description</span></div>',
 			'</div>'
 		].join('')
-	}
+	};
 }
 
 angular
